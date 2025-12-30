cb27f36d9897789fc7bc590337b3bc5149990324
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2025-12-30 02:37:58 +0100

    Try to fix wsd.hpp, see comments

--- wsd.hpp
+++ wsd.hpp
@@ -12,6 +12,7 @@
 #define WSD
 
 #include <atomic>
+#include <cstdlib>
 
 // TODO: I used T in expecting to receive Task*, should we switch to Task, and use T* instead ?
 // First structure defined in Figure 4: Growable Circular Array
@@ -24,10 +25,13 @@ private:
 
 public:
     CircularArray<T>(int log_size) : log_size(log_size) {
-        segment = new T[1 << log_size];
+        segment = (T*) malloc((1 << log_size) * sizeof(T*));
     }
     ~CircularArray() {
-        delete[] segment;// TODO: should we delete tasks as well
+        // NOTE: an idea would be to shallow free and free tasks from the task free list only  ??
+        free(segment);
+        segment = nullptr;
+        // TODO: should we delete tasks as well ???
     }
     long size() {
         return 1 << log_size;
@@ -38,10 +42,10 @@ public:
     void put(long i, T new_object) {
         segment[i % size()] = new_object;
     }
-    CircularArray grow(long bottom_index, long top_index) {
-        CircularArray a = CircularArray(log_size + 1);
+    CircularArray<T>* grow(long bottom_index, long top_index) {
+        CircularArray<T>* a = new CircularArray(log_size + 1);
         for (long i = top_index; i < bottom_index; i++) {
-            a.put(i, get(i));
+            a->put(i, get(i));
         }
         return a;
     }
@@ -59,49 +63,59 @@ private:
     long bottom = 0;
     std::atomic<long> top = 0;
     CircularArray<T> activeArray = CircularArray<T>(LogInitialSize);
+    // TODO: current segfault a probably caused by these issues here, race conditions between steal and pushbottom or grow(). when I remove the stealing part it seems segfault disappear
+    // by commenting line "nextTidToStealFrom = (nextTidToStealFrom + 1) % _nbThreads;" in wsd.hpp and running "./tsp dj38.tsp 14 10 8", this is segfaulting all the time for me
+    // TODO: refactor the management of activeArray to maybe have atomic pointers ? at least we don't want to do copies like CircularArray a = activeArray;
+    // this needs further understanding of the algo in the paper...
 
 public:
     void pushBottom(T o) {
+        // std::cout << "pushBottom of " << *o << std::endl;
         long b = bottom;
         long t = top;
-        CircularArray a = activeArray;
+        CircularArray<T>* a = &activeArray;
         long size = b - t;
-        if (size >= a.size() - 1) {
-            a = a.grow(b, t);
-            activeArray = a;
+        if (size >= a->size() - 1) {
+            a = a->grow(b, t);
+            activeArray = *a;
         }
-        a.put(b, o);
+        a->put(b, o);
         bottom = b + 1;
     }
 
     T popBottom() {
+        // std::cout << "popBottom\n";
         long b = bottom;
-        CircularArray a = activeArray;
+        CircularArray<T>* a = &activeArray;
         b = b - 1;
         bottom = b;
         long t = top;
         long size = b - t;
         if (size < 0) {
             bottom = t;
+            // std::cout << "pop got empty...\n";
             return Empty;
         }
-        T o = a.get(b);
+        T o = a->get(b);
         if (size > 0)
             return o;
-        if (!top.compare_exchange_strong(t, t + 1, std::memory_order_seq_cst))// TODO: strong or weak option ? memory order to specify or not ?
+        if (!top.compare_exchange_strong(t, t + 1))// TODO: strong or weak option ? memory order to specify or not ?
             o = Empty;
         bottom = t + 1;
+        // std::cout << "pop got -> ptr ";
+        // printf("%p\n", o);
         return o;
     }
 
     T steal() {
+        // std::cout << "steal !\n";
         long t = top;
         long b = bottom;
-        CircularArray a = activeArray;
+        CircularArray<T>* a = &activeArray;
         long size = b - t;
         if (size <= 0) return Empty;
-        T o = a.get(t);
-        if (!top.compare_exchange_strong(t, t + 1, std::memory_order_seq_cst))// TODO: strong or weak option ? memory order to specify or not ?
+        T o = a->get(t);
+        if (!top.compare_exchange_strong(t, t + 1))// TODO: strong or weak option ? memory order to specify or not ?
             return Abort;
         return o;
     }
